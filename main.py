40bec12dbcb803002cce2ebc8aa1adc00fdd0030
noteason <74929228+noteason@users.noreply.github.com>
2022-02-08 13:22:17 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import PIL
+from io import BytesIO
+import urllib.request
+from PIL import Image, ImageDraw, ImageFont
+import json
+import requests
+
+rq = requests.get("https://fortnite-api.com/v2/cosmetics/br")
+data=rq.json()['data']
+
+
+print("Generating Images... This May Take A While!")
+for item in data:
+  if "CID_" in item['id']:
+    rarity = item['rarity']['value']
+    background = Image.open(f"raritys/{rarity}.png")
+
+    url=f"https://fortnite-api.com/images/cosmetics/br/{item['id']}/icon.png"
+    response = urllib.request.urlopen(url)
+    foreground = Image.open(response)
+
+    background.paste(foreground, (0, 0), foreground)
+    background.save(f"skins/{item['id']}.png")
+print("Done Generating Images!")
